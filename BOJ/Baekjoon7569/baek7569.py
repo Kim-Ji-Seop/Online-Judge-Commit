6d692eb16cb0231a53dd4c8a0458f048b6b7fa1b
KIM-JI-SEOP <jskim2x@naver.com>
2022-04-03 15:49:19 +0900

    20220403

--- BOJ/Baekjoon7569/baek7569.py
+++ BOJ/Baekjoon7569/baek7569.py
@@ -3,45 +3,44 @@ from collections import deque
 
 
 M,N,H = map(int,stdin.readline().split())
-graph = [[list(map(int, stdin.readline().split())) for _ in range(M)] for _ in range(H)]
-for i in range(M*H):
-    for j in range(N):
-        graph[i][j]
+graph = [[list(map(int, stdin.readline().split())) for _ in range(N)] for _ in range(H)]
+
 cnt = 0
 X = [-1,1,0,0,0,0]
 Y = [0,0,-1,1,0,0]
 Z = [0,0,0,0,-1,1]
-print(graph)
-# def BFS():
-#     global cnt
-#     queue = deque()
-#     for i in range(M):
-#         for j in range(N):
-#             for k in range(H):
-#                 if graph[i][j][k] == 1:
-#                     queue.append((i,j,k))
-    
-#     while queue:
-#         x, y, z = queue.popleft()
-#         for i in range(6):
-#             nx = x + X[i]
-#             ny = y + Y[i]
-#             nz = z + Z[i]
-#             if nx < 0 or nx >= M or ny < 0 or ny >= N or nz < 0 or nz >= H:
-#                 continue
-#             if graph[nx][ny][nz] == -1:
-#                 continue
-#             if graph[nx][ny][nz] == 0:
-#                 graph[nx][ny][nz] = graph[x][y][z] + 1
-#                 queue.append((nx,ny,nz))
-            
-    
 
-# BFS()
-# for i in graph:
-#     for j in i:
-#         if j == 0:
-#             print(-1)
-#             exit(0)
-#     cnt = max(cnt,max(i))
-# print(cnt - 1)
+
+                    
+def BFS():
+    global cnt
+    queue = deque()
+    for i in range(H): #z
+        for j in range(N): #x
+            for k in range(M): #y
+                if graph[i][j][k] == 1:
+                    queue.append((i,j,k))
+    
+    while queue:
+        z, x, y = queue.popleft()
+        for i in range(6):
+            nz = z + Z[i]
+            nx = x + X[i]
+            ny = y + Y[i]
+            if nx < 0 or nx >= N or ny < 0 or ny >= M or nz < 0 or nz >= H:
+                continue
+            if graph[nz][nx][ny] == -1:
+                continue
+            if graph[nz][nx][ny] == 0:
+                graph[nz][nx][ny] = graph[z][x][y] + 1
+                queue.append((nz,nx,ny))
+
+BFS()
+for i in graph:
+    for j in i:
+        for k in j:
+            if k == 0:
+                print(-1)
+                exit(0)
+        cnt = max(cnt,max(j))
+print(cnt - 1)
